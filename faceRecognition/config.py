2dc5b789ce119de4737b926bccd69320accec12a
zhuyiling <yiling.zhu@realai.ai>
2020-03-20 18:23:12 +0800

    face Rec

---
+++ faceRecognition/config.py
@@ -0,0 +1,54 @@
+import torch
+import torchvision.transforms as T
+
+class Config:
+    # network settings
+    backbone = 'fmobile' # [resnet, fmobile]
+    metric = 'arcface'  # [cosface, arcface]
+    embedding_size = 512
+    drop_ratio = 0.5
+
+    # data preprocess
+    input_shape = [1, 128, 128]
+    train_transform = T.Compose([
+        T.Grayscale(),
+        T.RandomHorizontalFlip(),
+        T.Resize((144, 144)),
+        T.RandomCrop(input_shape[1:]),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5], std=[0.5]),
+    ])
+    test_transform = T.Compose([
+        T.Grayscale(),
+        T.Resize(input_shape[1:]),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5], std=[0.5]),
+    ])
+
+    # dataset
+    train_root = '/mnt/mfs/yiling/data/face/webface/CASIA-WebFace'
+    test_root = "/mnt/mfs/yiling/data/face/lfw/lfw-align-128"
+    test_list = "/mnt/mfs/yiling/data/face/lfw/lfw_test_pair.txt"
+    
+    # training settings
+    checkpoints = "checkpoints"
+    restore = False
+    restore_model = ""
+    test_model = "checkpoints/24.pth"
+    
+    train_batch_size = 256
+    test_batch_size = 60
+
+    epoch = 150
+    optimizer = 'sgd'  # ['sgd', 'adam']
+    lr = 1e-1
+    lr_step = 10
+    lr_decay = 0.95
+    weight_decay = 5e-4
+    loss = 'focal_loss' # ['focal_loss', 'cross_entropy']
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    pin_memory = True  # if memory is large, set it True to speed up a bit
+    num_workers = 4  # dataloader
+
+config = Config()
---
+++ faceRecognition/model/__init__.py
@@ -0,0 +1,3 @@
+from .fmobilenet import FaceMobileNet
+from .loss import FocalLoss
+from .metric import ArcFace, CosFace
