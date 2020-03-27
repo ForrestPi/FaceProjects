5c4f8e894f363269662a65cdd82b97a3bdcf6152
forrestpi <forrest_zhu@foxmail.com>
2020-03-27 20:13:04 +0800

    face.evoLVe.PyTorch

--- faceRecognition/utils.py
+++ faceRecognition/utils.py
@@ -1,63 +1,255 @@
-"""Train List 训练列表
-格式：
-ImagePath Label
-示例:
-/data/WebFace/0124920/003.jpg 10572
-/data/WebFace/0124920/012.jpg 10572
-/data/WebFace/0124920/020.jpg 10572
-"""
+import torch
+import torchvision.transforms as transforms
+import torch.nn.functional as F
 
+from .verification import evaluate
+
+from datetime import datetime
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import numpy as np
+from PIL import Image
+import bcolz
+import io
 import os
-import os.path as osp
-from imutils import paths
 
-def generate_list(images_directory, saved_name=None):
-    """生成数据列表
-    Args:
-        images_directory: 人脸数据目录，通常包含多个子文件夹。如
-            WebFace和LFW的格式
-    Returns:
-        data_list: [<路径> <标签>]
-    """
-    subdirs = os.listdir(images_directory)
-    num_ids = len(subdirs)
-    data_list = []
-    for i in range(num_ids):
-        subdir = osp.join(images_directory, subdirs[i])
-        files = os.listdir(subdir)
-        paths = [osp.join(subdir, file) for file in files]
-        # 添加ID作为其人脸标签
-        paths_with_Id = [f"{p} {i}\n" for p in paths]
-        data_list.extend(paths_with_Id)
+
+# Support: ['get_time', 'l2_norm', 'make_weights_for_balanced_classes', 'get_val_pair', 'get_val_data', 'separate_irse_bn_paras', 'separate_resnet_bn_paras', 'warm_up_lr', 'schedule_lr', 'de_preprocess', 'hflip_batch', 'ccrop_batch', 'gen_plot', 'perform_val', 'buffer_val', 'AverageMeter', 'accuracy']
+
+
+def get_time():
+    return (str(datetime.now())[:-10]).replace(' ', '-').replace(':', '-')
+
+
+def l2_norm(input, axis = 1):
+    norm = torch.norm(input, 2, axis, True)
+    output = torch.div(input, norm)
+
+    return output
+
+
+def make_weights_for_balanced_classes(images, nclasses):
+    '''
+        Make a vector of weights for each image in the dataset, based
+        on class frequency. The returned vector of weights can be used
+        to create a WeightedRandomSampler for a DataLoader to have
+        class balancing when sampling for a training batch.
+            images - torchvisionDataset.imgs
+            nclasses - len(torchvisionDataset.classes)
+        https://discuss.pytorch.org/t/balanced-sampling-between-classes-with-torchvision-dataloader/2703/3
+    '''
+    count = [0] * nclasses
+    for item in images:
+        count[item[1]] += 1  # item is (img-data, label-id)
+    weight_per_class = [0.] * nclasses
+    N = float(sum(count))  # total number of images
+    for i in range(nclasses):
+        weight_per_class[i] = N / float(count[i])
+    weight = [0] * len(images)
+    for idx, val in enumerate(images):
+        weight[idx] = weight_per_class[val[1]]
+
+    return weight
+
+
+def get_val_pair(path, name):
+    carray = bcolz.carray(rootdir = os.path.join(path, name), mode = 'r')
+    issame = np.load('{}/{}_list.npy'.format(path, name))
+
+    return carray, issame
+
+
+def get_val_data(data_path):
+    lfw, lfw_issame = get_val_pair(data_path, 'lfw')
+    cfp_ff, cfp_ff_issame = get_val_pair(data_path, 'cfp_ff')
+    cfp_fp, cfp_fp_issame = get_val_pair(data_path, 'cfp_fp')
+    agedb_30, agedb_30_issame = get_val_pair(data_path, 'agedb_30')
+    calfw, calfw_issame = get_val_pair(data_path, 'calfw')
+    cplfw, cplfw_issame = get_val_pair(data_path, 'cplfw')
+    vgg2_fp, vgg2_fp_issame = get_val_pair(data_path, 'vgg2_fp')
+
+    return lfw, cfp_ff, cfp_fp, agedb_30, calfw, cplfw, vgg2_fp, lfw_issame, cfp_ff_issame, cfp_fp_issame, agedb_30_issame, calfw_issame, cplfw_issame, vgg2_fp_issame
+
+
+def separate_irse_bn_paras(modules):
+    if not isinstance(modules, list):
+        modules = [*modules.modules()]
+    paras_only_bn = []
+    paras_wo_bn = []
+    for layer in modules:
+        if 'model' in str(layer.__class__):
+            continue
+        if 'container' in str(layer.__class__):
+            continue
+        else:
+            if 'batchnorm' in str(layer.__class__):
+                paras_only_bn.extend([*layer.parameters()])
+            else:
+                paras_wo_bn.extend([*layer.parameters()])
+
+    return paras_only_bn, paras_wo_bn
+
+
+def separate_resnet_bn_paras(modules):
+    all_parameters = modules.parameters()
+    paras_only_bn = []
+
+    for pname, p in modules.named_parameters():
+        if pname.find('bn') >= 0:
+            paras_only_bn.append(p)
+            
+    paras_only_bn_id = list(map(id, paras_only_bn))
+    paras_wo_bn = list(filter(lambda p: id(p) not in paras_only_bn_id, all_parameters))
     
-    if saved_name:
-        with open(saved_name, 'w', encoding='utf-8') as f:
-            f.writelines(data_list)
-    return data_list
+    return paras_only_bn, paras_wo_bn
 
-def transform_clean_list(webface_directory, cleaned_list_path):
-    """转换webface的干净列表格式
-    Args:
-        webface_directory: WebFace数据目录
-        cleaned_list_path: cleaned_list.txt路径
-    Returns:
-        cleaned_list: 转换后的数据列表
-    """
-    with open(cleaned_list_path, encoding='utf-8') as f:
-        cleaned_list = f.readlines()
-    cleaned_list = [p.replace('\\', '/') for p in cleaned_list]
-    cleaned_list = [osp.join(webface_directory, p) for p in cleaned_list]
-    return cleaned_list
 
-def remove_dirty_image(webface_directory, cleaned_list):
-    cleaned_list = set([c.split()[0] for c in cleaned_list])
-    for p in paths.list_images(webface_directory):
-        if p not in cleaned_list:
-            print(f"remove {p}")
-            os.remove(p)
+def warm_up_lr(batch, num_batch_warm_up, init_lr, optimizer):
+    for params in optimizer.param_groups:
+        params['lr'] = batch * init_lr / num_batch_warm_up
 
-if __name__ == '__main__':
-    data = '/mnt/mfs/yiling/data/face/webface/CASIA-WebFace'
-    lst = '/mnt/mfs/yiling/data/face/webface/cleaned_list.txt'
-    cleaned_list = transform_clean_list(data, lst)
-    remove_dirty_image(data, cleaned_list)
+    # print(optimizer)
+
+
+def schedule_lr(optimizer):
+    for params in optimizer.param_groups:
+        params['lr'] /= 10.
+
+    print(optimizer)
+
+
+def de_preprocess(tensor):
+
+    return tensor * 0.5 + 0.5
+
+
+hflip = transforms.Compose([
+            de_preprocess,
+            transforms.ToPILImage(),
+            transforms.functional.hflip,
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ])
+
+
+def hflip_batch(imgs_tensor):
+    hfliped_imgs = torch.empty_like(imgs_tensor)
+    for i, img_ten in enumerate(imgs_tensor):
+        hfliped_imgs[i] = hflip(img_ten)
+
+    return hfliped_imgs
+
+
+ccrop = transforms.Compose([
+            de_preprocess,
+            transforms.ToPILImage(),
+            transforms.Resize([128, 128]),  # smaller side resized
+            transforms.CenterCrop([112, 112]),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ])
+
+
+def ccrop_batch(imgs_tensor):
+    ccropped_imgs = torch.empty_like(imgs_tensor)
+    for i, img_ten in enumerate(imgs_tensor):
+        ccropped_imgs[i] = ccrop(img_ten)
+
+    return ccropped_imgs
+
+
+def gen_plot(fpr, tpr):
+    """Create a pyplot plot and save to buffer."""
+    plt.figure()
+    plt.xlabel("FPR", fontsize = 14)
+    plt.ylabel("TPR", fontsize = 14)
+    plt.title("ROC Curve", fontsize = 14)
+    plot = plt.plot(fpr, tpr, linewidth = 2)
+    buf = io.BytesIO()
+    plt.savefig(buf, format = 'jpeg')
+    buf.seek(0)
+    plt.close()
+
+    return buf
+
+
+def perform_val(multi_gpu, device, embedding_size, batch_size, backbone, carray, issame, nrof_folds = 10, tta = True):
+    if multi_gpu:
+        backbone = backbone.module # unpackage model from DataParallel
+        backbone = backbone.to(device)
+    else:
+        backbone = backbone.to(device)
+    backbone.eval() # switch to evaluation mode
+
+    idx = 0
+    embeddings = np.zeros([len(carray), embedding_size])
+    with torch.no_grad():
+        while idx + batch_size <= len(carray):
+            batch = torch.tensor(carray[idx:idx + batch_size][:, [2, 1, 0], :, :])
+            if tta:
+                ccropped = ccrop_batch(batch)
+                fliped = hflip_batch(ccropped)
+                emb_batch = backbone(ccropped.to(device)).cpu() + backbone(fliped.to(device)).cpu()
+                embeddings[idx:idx + batch_size] = l2_norm(emb_batch)
+            else:
+                ccropped = ccrop_batch(batch)
+                embeddings[idx:idx + batch_size] = l2_norm(backbone(ccropped.to(device))).cpu()
+            idx += batch_size
+        if idx < len(carray):
+            batch = torch.tensor(carray[idx:])
+            if tta:
+                ccropped = ccrop_batch(batch)
+                fliped = hflip_batch(ccropped)
+                emb_batch = backbone(ccropped.to(device)).cpu() + backbone(fliped.to(device)).cpu()
+                embeddings[idx:] = l2_norm(emb_batch)
+            else:
+                ccropped = ccrop_batch(batch)
+                embeddings[idx:] = l2_norm(backbone(ccropped.to(device))).cpu()
+
+    tpr, fpr, accuracy, best_thresholds = evaluate(embeddings, issame, nrof_folds)
+    buf = gen_plot(fpr, tpr)
+    roc_curve = Image.open(buf)
+    roc_curve_tensor = transforms.ToTensor()(roc_curve)
+
+    return accuracy.mean(), best_thresholds.mean(), roc_curve_tensor
+
+
+def buffer_val(writer, db_name, acc, best_threshold, roc_curve_tensor, epoch):
+    writer.add_scalar('{}_Accuracy'.format(db_name), acc, epoch)
+    writer.add_scalar('{}_Best_Threshold'.format(db_name), best_threshold, epoch)
+    writer.add_image('{}_ROC_Curve'.format(db_name), roc_curve_tensor, epoch)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val   = 0
+        self.avg   = 0
+        self.sum   = 0
+        self.count = 0
+
+    def update(self, val, n = 1):
+        self.val   = val
+        self.sum   += val * n
+        self.count += n
+        self.avg   = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred    = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+
+    return res
